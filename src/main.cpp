8a9b4c6bd1b680b59a221f07ed6b110d97cc82b6
pavlosm12 <95383090+pavlosm12@users.noreply.github.com>
2022-08-08 22:35:28 +0300

    v4

--- src/main.cpp
+++ src/main.cpp
@@ -20,16 +20,14 @@ void setup() {
     // Connect to I2C bus as master
   Wire.begin();
   // put your setup code here, to run once:
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
+  pinMode(pump, OUTPUT);
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
   
 
 shtc3_setup();  //shtc3
 tsl2561_setup();
-Ultrasonic_sensor_setup();
+//Ultrasonic_sensor_setup();
   
   attachInterrupt(0,handleInt,FALLING); // Attach Interrupt to pin D2
 
@@ -47,49 +45,15 @@ void loop() {
  // -------------------- Receive Bluetooth signal ----------------------
  test12();
  mySerial_check();
-  // put your main code here, to run repeatedly:
+ pumpTimings();
 
-
-
-
-//analogWrite(led3, pwmLed);
-if (Auto_EN_DIS == 0){
-
-digitalWrite(led1,Manual_ON_OFF);
-}
-
-
-
-
-
-// PUMP AUTO/MANUAL BUTTONS LOGIC //
- 
-if ( Auto_EN_DIS !=0 )
-{
-
-  SM_pump();
-}
-
-if (Auto_EN_DIS != 1)
-{
-  if (Manual_ON_OFF != 0)
-  {
-    digitalWrite(4,Manual_ON_OFF); //insert pump pin here 
-  }
-}
-// PUMP AUTO/MANUAL BUTTONS LOGIC //
-print_time ();
+//print_time ();
 
 //SM_led1();
-SM_pump ();
-Ultrasonic_sensor_readings();
-pressure();
-SM_writeEEPROM();
-SM_readEEPROM();
-print_state_writeEEPROM ();
-print_state_readEEPROM ();
-
-analogWrite(led2,pwmLed);
-
+//Ultrasonic_sensor_readings();
+//SM_writeEEPROM();
+//SM_readEEPROM();
+//print_state_writeEEPROM ();
+//print_state_readEEPROM ();
 
 }
