9fa523526268f9a7b7e31f28555819b7e5cb6555
pavlosm12 <95383090+pavlosm12@users.noreply.github.com>
2022-08-08 22:35:11 +0300

    v4

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,7 @@
 #include <time.h>
 #include <SoftwareSerial.h>
 #include <led_schedule>
-
-
-
+#include <EEPROM.h>
 
 
 // Interrupt Handler
@@ -17,30 +15,29 @@ void handleInt()
 }
 
 
-
 void setup() {
+
+    // Connect to I2C bus as master
+  Wire.begin();
   // put your setup code here, to run once:
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
-  pinMode(photoResistorPin, INPUT);
-  pinMode(tmp36_Pin, INPUT);
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
   
-  // Attach Interrupt to pin D2
-  attachInterrupt(0,handleInt,FALLING);
+
+shtc3_setup();  //shtc3
+tsl2561_setup();
+Ultrasonic_sensor_setup();
+  
+  attachInterrupt(0,handleInt,FALLING); // Attach Interrupt to pin D2
+
   DS3132_p_setup_adjust();
   DS3132_p_setup_sqw();
-
   mySerial.begin(9600);
   Serial.begin(9600);
-  Serial.println("DHTxx test!");
-  dht.begin();
 }
-
-
-
 
 
 
@@ -56,36 +53,43 @@ void loop() {
 
 
 //analogWrite(led3, pwmLed);
-if (blink_enable == 0){
+if (Auto_EN_DIS == 0){
 
-digitalWrite(led1,pump_button);
+digitalWrite(led1,Manual_ON_OFF);
 }
 
 
 
-/*
-if(millis() >= time_now + period)
+
+
+// PUMP AUTO/MANUAL BUTTONS LOGIC //
+ 
+if ( Auto_EN_DIS !=0 )
 {
-        time_now += period;
-        Serial.print("Photo resistor: ");
-        Serial.print(photoValue);
-        Serial.println(" %");
-        Serial.print("Temperature = ");
-        Serial.print(tmp36_temperature);
-        
 
+  SM_pump();
 }
- */
 
+if (Auto_EN_DIS != 1)
+{
+  if (Manual_ON_OFF != 0)
+  {
+    digitalWrite(4,Manual_ON_OFF); //insert pump pin here 
+  }
+}
+// PUMP AUTO/MANUAL BUTTONS LOGIC //
 print_time ();
 
-SM_led1();
+//SM_led1();
 SM_pump ();
-print_state_led1 ();
-print_state_pump ();
+Ultrasonic_sensor_readings();
+pressure();
+SM_writeEEPROM();
+SM_readEEPROM();
+print_state_writeEEPROM ();
+print_state_readEEPROM ();
 
-
-
+analogWrite(led2,pwmLed);
 
 
 }
